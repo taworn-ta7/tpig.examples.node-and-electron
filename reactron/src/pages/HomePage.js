11a932fafd5905dcbb3d9398eed1d2f4313ddc4c
Taworn Ta <taworn.ta7@gmail.com>
2021-03-12 12:04:59 +0700

    - more react app

---
+++ reactron/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = (props) => {
+    // rendering
+    return (
+        <div>
+            Hello, world :)
+        </div>
+    )
+}
+
+export default HomePage
